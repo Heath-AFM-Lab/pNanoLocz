fa6c778e5ffcdeba8d172753748d07bfc3acaa80
Onaib Faisal <onaibfaisal@Onaibs-MacBook-Pro.local>
2024-07-10 15:19:17 +0100

    Attempt at folder pane

--- src/UI_components/LHS_Components/File_System_Module.py
+++ src/UI_components/LHS_Components/File_System_Module.py
@@ -1,95 +1,120 @@
 import os
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QPushButton, QCheckBox
-from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import (QWidget, QHBoxLayout, QPushButton, QCheckBox, QVBoxLayout, QFileDialog, QTreeView, QDialog, QDialogButtonBox, QLabel)
+from PyQt6.QtGui import QIcon, QFileSystemModel
+from PyQt6.QtCore import Qt
+
 from constants import PATH_TO_ICON_DIRECTORY
 
 class FileSystemWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.buildFileManagementIcons()
-    
+        self.buildFileExplorerPane()
+
     def buildFileManagementIcons(self):
-        # Create a QHBoxLayout to contain the icons
         fileManagementLayout = QHBoxLayout()
 
-        # Add Autosave Checkbox
         self.autosaveCheckbox = QCheckBox("Autosave")
         self.autosaveCheckbox.setToolTip("Enable or disable autosave")
         fileManagementLayout.addWidget(self.autosaveCheckbox)
 
-        # Add Save Icon
         self.saveButton = QPushButton()
         self.saveButton.setIcon(QIcon(os.path.join(PATH_TO_ICON_DIRECTORY, "save.png")))
-        self.saveButton.setIconSize(self.saveButton.sizeHint())  # Adjust icon size to button size
+        self.saveButton.setIconSize(self.saveButton.sizeHint())
         self.saveButton.setToolTip("Save")
         self.saveButton.setFixedSize(self.saveButton.sizeHint())
         fileManagementLayout.addWidget(self.saveButton)
 
-        # Add Open Folder icon
         self.openFolderButton = QPushButton()
         self.openFolderButton.setIcon(QIcon(os.path.join(PATH_TO_ICON_DIRECTORY, "open.png")))
-        self.openFolderButton.setIconSize(self.openFolderButton.sizeHint())  # Adjust icon size to button size
+        self.openFolderButton.setIconSize(self.openFolderButton.sizeHint())
         self.openFolderButton.setToolTip("Open Folder")
         self.openFolderButton.setFixedSize(self.openFolderButton.sizeHint())
         fileManagementLayout.addWidget(self.openFolderButton)
 
-        # Add Navigate out of Directory icon
         self.navigateOutOfDirectoryButton = QPushButton()
         self.navigateOutOfDirectoryButton.setIcon(QIcon(os.path.join(PATH_TO_ICON_DIRECTORY, "up.png")))
-        self.navigateOutOfDirectoryButton.setIconSize(self.navigateOutOfDirectoryButton.sizeHint())  # Adjust icon size to button size
+        self.navigateOutOfDirectoryButton.setIconSize(self.navigateOutOfDirectoryButton.sizeHint())
         self.navigateOutOfDirectoryButton.setToolTip("Navigate out of Directory")
         self.navigateOutOfDirectoryButton.setFixedSize(self.navigateOutOfDirectoryButton.sizeHint())
         fileManagementLayout.addWidget(self.navigateOutOfDirectoryButton)
 
-        # Add Navigate into Directory icon
         self.navigateIntoDirectoryButton = QPushButton()
         self.navigateIntoDirectoryButton.setIcon(QIcon(os.path.join(PATH_TO_ICON_DIRECTORY, "down.png")))
-        self.navigateIntoDirectoryButton.setIconSize(self.navigateIntoDirectoryButton.sizeHint())  # Adjust icon size to button size
+        self.navigateIntoDirectoryButton.setIconSize(self.navigateIntoDirectoryButton.sizeHint())
         self.navigateIntoDirectoryButton.setToolTip("Navigate into Directory")
         self.navigateIntoDirectoryButton.setFixedSize(self.navigateIntoDirectoryButton.sizeHint())
         fileManagementLayout.addWidget(self.navigateIntoDirectoryButton)
 
-        # Add stretch to push all widgets to LHS
         fileManagementLayout.addStretch(1)
 
-        # Set layout to the current widget
-        self.setLayout(fileManagementLayout)
+        # Create a main layout and add both file management icons and file explorer pane
+        mainLayout = QVBoxLayout(self)
+        mainLayout.addLayout(fileManagementLayout)
+        self.setLayout(mainLayout)
 
-        # Set names and signals of all buttons and checkbox
         self.autosaveCheckbox.clicked.connect(self.onAutosaveClicked)
         self.saveButton.clicked.connect(self.onSaveButtonClicked)
         self.openFolderButton.clicked.connect(self.onOpenFolderButtonClicked)
         self.navigateOutOfDirectoryButton.clicked.connect(self.onNavigateOutButtonClicked)
         self.navigateIntoDirectoryButton.clicked.connect(self.onNavigateInButtonClicked)
 
+    def buildFileExplorerPane(self):
+        self.fileTreeView = QTreeView()
+        self.fileSystemModel = QFileSystemModel()
+        self.fileSystemModel.setRootPath('')
+        self.fileTreeView.setModel(self.fileSystemModel)
+        self.fileTreeView.setRootIndex(self.fileSystemModel.index(''))
+
+        # Adjust column width for better file name display
+        self.fileTreeView.setColumnWidth(0, 250)  # Adjust the width as needed
+
+        # Add the file tree view to the main layout
+        self.layout().addWidget(self.fileTreeView)
+
     def onAutosaveClicked(self):
-        # TODO: Complete Autosave function
         pass
 
     def onSaveButtonClicked(self):
-        # TODO: Complete save button function
-        pass
-        
-    def onOpenFolderButtonClicked(self):
-        # TODO: Complete Open Folder function
-        pass
-    
-    def onNavigateInButtonClicked(self):
-        # TODO: Complete Navigate Into file function
-        pass
-    
-    def onNavigateOutButtonClicked(self):
-        # TODO: Complete Navigate Into file function
         pass
 
-# TODO: remove once finished
-from PyQt6.QtWidgets import QApplication
-import sys
+    def onOpenFolderButtonClicked(self):
+        dialog = QDialog(self)
+        dialog.setWindowTitle("Select a folder or a session file")
+
+        layout = QVBoxLayout()
+        label = QLabel("Select a folder or a session file")
+        layout.addWidget(label)
+
+        buttonBox = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+        buttonBox.accepted.connect(lambda: self.openFolder(dialog))
+        buttonBox.rejected.connect(dialog.reject)
+
+        layout.addWidget(buttonBox)
+        dialog.setLayout(layout)
+        dialog.exec()
+
+    def openFolder(self, dialog):
+        dialog.accept()
+        folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
+        if folder_path:
+            print(f"Selected folder: {folder_path}")
+            self.populateFileTree(folder_path)
+
+    def populateFileTree(self, folder_path):
+        print(f"Populating file tree with: {folder_path}")
+        self.fileSystemModel.setRootPath(folder_path)
+        self.fileTreeView.setRootIndex(self.fileSystemModel.index(folder_path))
+
+    def onNavigateInButtonClicked(self):
+        pass
+
+    def onNavigateOutButtonClicked(self):
+        pass
+
 
 if __name__ == '__main__':
-    # TODO: remove once format correction is complete
     ICON_DIRECTORY = "../../../assets/icons"
-    # Path to icon directory
     PATH_TO_ICON_DIRECTORY = os.path.abspath(os.path.join(os.getcwd(), ICON_DIRECTORY))
     app = QApplication(sys.argv)
     file_system = FileSystemWidget()
